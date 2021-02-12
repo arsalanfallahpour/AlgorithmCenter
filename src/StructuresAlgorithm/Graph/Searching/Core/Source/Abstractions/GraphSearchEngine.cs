d2a3ad0455798544ef0e36d355a1eea815f7c7f0
arsalan falahpour <arsalan.falahpour@outlook.com>
2021-02-12 20:29:42 +0330

    Create GraphSearchEngine abstract base class

---
+++ src/StructuresAlgorithm/Graph/Searching/Core/Source/Abstractions/GraphSearchEngine.cs
@@ -0,0 +1,12 @@
+﻿namespace AlgorithmCenter.StructuresAlgorithm.Graph.Searching.Core.Abstractions
+{
+
+    using AlgorithmCenter.StructuresAlgorithm.Graph.Core.Abstractions;
+    public abstract class GraphSearchEngine<TNode>
+    {
+        public GraphSearchEngine(ListGraph<TNode> graph) => _graph = graph;
+        public ListGraph<TNode> Graph => _graph; 
+        private readonly ListGraph<TNode> _graph;
+        public abstract bool HasPath(TNode sourceValue,TNode destinationValue);
+    }
+}
